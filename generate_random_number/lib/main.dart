f13035178e1702ccfdfe78ad681edc844db090e9
imonursahin <imonursahin@gmail.com>
2021-05-09 21:19:12 +0300

    main

--- generate_random_number/lib/main.dart
+++ generate_random_number/lib/main.dart
@@ -1,3 +1,40 @@
 import 'package:flutter/material.dart';
 
 import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void randomNumber() {
+    var random = new Random();
+
+    int minValue = 10;
+    int maxValue = 200;
+    int result = minValue + random.nextInt(maxValue - minValue);
+
+    print(result);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+          Container(
+              margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: RaisedButton(
+                onPressed: () => randomNumber(),
+                child: Text(
+                  'Click for Generate Number',
+                  style: TextStyle(fontSize: 20),
+                ),
+                textColor: Colors.white,
+                color: Colors.black,
+                padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+              )),
+        ]))));
+  }
+}
