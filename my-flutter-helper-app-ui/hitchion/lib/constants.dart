9389e083b33552b735dbcf5ef93220fe8d6a9492
imonursahin <imonursahin@gmail.com>
2021-05-25 11:47:58 +0300

    ui

---
+++ my-flutter-helper-app-ui/hitchion/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF263238);
+const kPrimaryLightColor = Color(0xFFECEFF1);
+const contactsDelete = Color(0xFFe53935);
+const safeIcon = Color(0xFF263238);
